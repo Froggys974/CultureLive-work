5f52f2d957805dd3b78f2a80b16f0c9e543e0e04
Grondin Florent <florent.grondin97@gmail.com>
2024-12-06 03:55:17 +0100

    add package and class validator

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,21 @@ import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CustomerModule } from './customer/customer.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { getTypeOrmConfig } from './config/typeorm.config';
 import { RentalModule } from './rentals/rental.module';
+import { ScheduledTaskModule } from './scheduled_task/scheduled_task.module';
+import { createTypeOrmConfig } from './config/data-source.config';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [ConfigModule.forRoot(), CustomerModule,
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: getTypeOrmConfig,
+      inject: [ConfigService], 
+      useFactory: (configService: ConfigService) => createTypeOrmConfig(configService),
     }),
     RentalModule,
+    ScheduledTaskModule,
+    ScheduleModule.forRoot(),
   ],
   controllers: [AppController],
   providers: [AppService],
